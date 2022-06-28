3908f48c72c8b18875f17f159fb46c8cf6f6c54a
tukasa <497116162@qq.com>
2022-06-28 21:28:23 +0800

    完成了作业4

---
+++ 作业四.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" name="" id="" :value="iptdata" @input="fn">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      iptdata:''
+    }
+  },
+  methods: {
+    fn(e) {
+      this.iptdata = e.target.value
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
